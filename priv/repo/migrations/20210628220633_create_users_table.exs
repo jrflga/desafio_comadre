548badc7a791675443a8b1048f959255b35acc53
João Ricardo <joao.r.alves@rededor.com.br>
2021-06-28 20:35:43 -0300

    Create user and account (model and migration)

---
+++ priv/repo/migrations/20210628220633_create_users_table.exs
@@ -0,0 +1,21 @@
+defmodule ComadrePay.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name,  :string
+      add :last_name, :string
+      add :cpf, :string
+      add :email, :string
+      add :password_hash, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:cpf])
+    create unique_index(:users, [:email])
+
+  end
+end
